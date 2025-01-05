73ad550e8192c069e9cfc98617b18da859a6689b
jihye <lovinsurfaces@gmail.com>
2025-01-06 00:29:54 +0900

    #7.3 Movie App part One 강의 파일 업로드

--- src/App.js
+++ src/App.js
@@ -29,9 +29,30 @@ function App() {
 
   return (
     <div>
-      {loading ? <p>Loading...</p> : null}
+      {loading ? <p>Loading...</p> : (
+        <div className="wrap">
+          {movies.map(movie => (
+            <div key={movie.id}>
+              <div className="thumb-img-box">
+                <img src={movie.large_cover_image} />
+              </div>
+              <p className="movie-tit">{movie.title} ({movie.year})</p>
+              <ul className="movie-genres">
+                {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+              </ul>
+              <p className="movie-desc">{movie.summary}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+/*
+  {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+  여기에서 movie.genres의 배열에는 id 등 어떤 고유값이 없기 때문에 원한다면 genre를 줄 수 있음 (genre가 고유한 값이 한 괜찮음)
+*/
+// ⭐️ map 함수 사용 시 key값을 꼭 부여해야 한다는 점 잊지 말기!
