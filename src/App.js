afbeae62a4531377123e01ae2104e918e0dff43d
jihye <lovinsurfaces@gmail.com>
2025-01-05 23:54:32 +0900

    #7.3 Movie App part One 강의 파일 업로드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json();
+
+    setLoading(false);
+    setMovies(json.data.movies)
+  };
+
+  useEffect(() => {
+    getMovies();
+
+    /*
+      fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+        .then((response) => response.json())
+        .then((json) => {
+          setLoading(false);
+          setMovies(json.data.movies)
+        });
+
+      ➡️ 요즈음은 then보다 async, await를 더 보편적으로 사용함
+    */
+  }, []);
+
+  console.log(movies);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <p>Loading...</p> : null}
     </div>
   );
 }
